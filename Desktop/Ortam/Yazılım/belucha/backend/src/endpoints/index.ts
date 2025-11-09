1b3c7a69bc354cf943532839f6e985262111cf92
Murathan Cotuk <160719343+murathan-cotuk@users.noreply.github.com>
2025-11-09 18:18:08 +0100

    Delete Desktop/Ortam/Yazılım/belucha directory

--- Desktop/Ortam/Yazılım/belucha/backend/src/endpoints/index.ts
+++
@@ -1,6 +0,0 @@
-// API Endpoints index
-export { default as checkoutRouter } from './checkout';
-export { default as webhookRouter } from './webhook';
-export { default as authRouter } from './auth';
-
-
